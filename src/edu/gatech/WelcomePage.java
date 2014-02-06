db870c736c16d4835d977031a5632f8f762e5aa4
kanaganai <kanaganai@gmail.com>
2014-02-05 20:50:32 -0500

    Login and Welcome Pages Created

--- src/edu/gatech/WelcomePage.java
+++ src/edu/gatech/WelcomePage.java
@@ -1,16 +1,33 @@
 package edu.gatech;
 
+import main.java.edu.gatech.LoginActivity;
 import main.java.edu.gatech.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.DialogInterface.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
 
-public class WelcomePage extends Activity {
+public class WelcomePage extends Activity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome-page);
+        setContentView(R.layout.activity_welcome_page);
+        Button btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		Log.i("clicks","You Clicked B1");
+                Intent i=new Intent(
+                       WelcomePage.this,
+                       LoginActivity.class);
+                startActivity(i);
+        	}
+        });
     }
 
 
@@ -20,5 +37,11 @@ public class WelcomePage extends Activity {
         getMenuInflater().inflate(R.menu.welcome_page, menu);
         return true;
     }
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
